d350ab52c8d7c1a15a0914b631c74c708527bebc
Dmitry Ronzhin <d_rongin@mail.ru>
2015-03-25 21:06:59 +0300

    First commit - nothing interesting to see

---
+++ mydb.h
@@ -0,0 +1,77 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Record {
+    size_t key_size;
+    void * key;
+    size_t data_size;
+    void * data;
+};
+
+struct BNode {
+    char is_leaf;
+    size_t page_num;
+    size_t pairs_num;
+    size_t parent;
+    struct Record* records;
+    size_t * links;
+};
+
+/* check `man dbopen` */
+struct DBT {
+	void  *data;
+	size_t size;
+};
+
+struct DB {
+	/* Public API */
+	/* Returns 0 on OK, -1 on Error */
+	int (*close)(struct DB *db);
+	int (*delete)(struct DB *db, struct DBT *key);
+	int (*insert)(struct DB *db, struct DBT *key, struct DBT *data);
+	/* * * * * * * * * * * * * *
+	 * Returns malloc'ed data into 'struct DBT *data'.
+	 * Caller must free data->data. 'struct DBT *data' must be alloced in
+	 * caller.
+	 * * * * * * * * * * * * * */
+	int (*select)(struct DB *db, struct DBT *key, struct DBT *data);
+	/* Sync cached pages with disk
+	 * */
+	int (*sync)(struct DB *db);
+	/* For future uses - sync cached pages with disk
+	 * int (*sync)(const struct DB *db)
+	 * */
+	/* Private API */
+	/*     ...     */
+        char *db_filename;
+        FILE * db_file;
+        struct DBC * conf;
+        size_t root_page_num;
+}; /* Need for supporting multiple backends (HASH/BTREE) */
+
+struct DBC {
+	/* Maximum on-disk file size
+	 * 512MB by default
+	 * */
+	size_t db_size;
+	/* Page (node/data) size
+	 * 4KB by default
+	 * */
+	size_t page_size;
+	/* Maximum cached memory size
+	 * 16MB by default
+	 * */
+	size_t cache_size;
+};
+
+/* Open DB if it exists, otherwise create DB */
+struct DB *dbopen(char *file, struct DBC *conf);
+
+int db_close(struct DB *db);
+int db_delete(struct DB *, void *, size_t);
+int db_select(struct DB *, void *, size_t, void **, size_t *);
+int db_insert(struct DB *, void *, size_t, void * , size_t  );
+
+/* Sync cached pages with disk */
+int db_sync(const struct DB *db);
